29acdda09c4753abbf72049daa93ba03ed8d097f
Sebastian <SebastianBittmann@gmail.com>
2012-10-11 19:25:48 +0100

    changed the port

--- com.ceteva.xmf/bin/createimages.sh
+++ com.ceteva.xmf/bin/createimages.sh
@@ -2,7 +2,7 @@
 clear
 DIR=$1
 echo "DIR is "$DIR
-PORT=100
+PORT=2100
 XMFHOME=$DIR
 LIB=$XMFHOME
 EVALUATOR=$XMFHOME/Images/eval.img
@@ -22,7 +22,7 @@ SERVERFILENAME=Boot/Server/Boot.o
 MOSAICMAKEFILENAME=Boot/Makefile.o
 #wont work
 echo "xos.img, derived from xos.bat"
-java $MAXJAVAHEAP $MAXJAVASTACKSIZE -cp $LIB XOS.OperatingSystem -port $PORT -image $EVALUATORIMAGE -heapSize $HEAPSIZE -freeHeap $FREEHEAP -stackSize $STACKSIZE -arg filename:$XOSFILENAME -arg user:"$USERNAME" -arg home:"$XMFHOME" -arg license:license.lic -arg projects:"$XMFPROJECTS" -arg doc:"$XMFDOC" -arg version:"$VERSION" %3 %4 %5 %6 %7 %8 %9
+java $MAXJAVAHEAP $MAXJAVASTACKSIZE -cp $LIB XOS.OperatingSystem -port $PORT -image $EVALUATORIMAGE -heapSize $HEAPSIZE -freeHeap $FREEHEAP -stackSize $STACKSIZE -arg filename:$XOSFILENAME -arg user:"$USERNAME" -arg home:"$XMFHOME" -arg license:license.lic -arg projects:"$XMFPROJECTS" -arg doc:"$XMFDOC" -arg version:"$VERSION"
 echo "Done xos"
 #works
 echo "eval.img"
@@ -34,12 +34,13 @@ java -cp $LIB XOS.OperatingSystem -port $PORT -initFile $DIR/Boot/Boot.o -image 
 echo "Done xmf"
 #works
 echo "server.img, derived from makexmfs.bat"
-java $MAXJAVAHEAP $MAXJAVASTACKSIZE -cp $LIB XOS.OperatingSystem -port $PORT -image $EVALUATORIMAGE -heapSize $HEAPSIZE -freeHeap $FREEHEAP -stackSize $STACKSIZE -arg filename:$SERVERFILENAME -arg user:"$USERNAME" -arg home:"$XMFHOME" -arg license:license.lic -arg projects:"$XMFPROJECTS" -arg doc:"$XMFDOC" -arg version:"$VERSION" %3 %4 %5 %6 %7 %8 %9
+java $MAXJAVAHEAP $MAXJAVASTACKSIZE -cp $LIB XOS.OperatingSystem -port $PORT -image $EVALUATORIMAGE -heapSize $HEAPSIZE -freeHeap $FREEHEAP -stackSize $STACKSIZE -arg filename:$SERVERFILENAME -arg user:"$USERNAME" -arg home:"$XMFHOME" -arg license:license.lic -arg projects:"$XMFPROJECTS" -arg doc:"$XMFDOC" -arg version:"$VERSION"
 echo "Done server"
 #wont work
 echo "Makefile mosaic.img"
-java $MAXJAVAHEAP -cp $LIB XOS.OperatingSystem -port $PORT -image $SERVERIMAGE -heapSize $HEAPSIZE -freeHeap $FREEHEAP -stackSize $STACKSIZE -arg filename:$MOSAICMAKEFILENAME -arg user:"$USERNAME" -arg home:"$XMFHOME" -arg license:license.lic -arg projects:"$XMFPROJECTS" -arg doc:"$XMFDOC" -arg version:"$VERSION" %3 %4 %5 %6 %7 %8 %9
+java $MAXJAVAHEAP -cp $LIB XOS.OperatingSystem -port $PORT -image $SERVERIMAGE -heapSize $HEAPSIZE -freeHeap $FREEHEAP -stackSize $STACKSIZE -arg filename:$MOSAICMAKEFILENAME -arg user:"$USERNAME" -arg home:"$XMFHOME" -arg license:license.lic -arg projects:"$XMFPROJECTS" -arg doc:"$XMFDOC" -arg version:"$VERSION"
+echo "Done makefile mosaic"
 #works
 echo "Compile mosaic.img"
-java $MAXJAVAHEAP -cp $LIB XOS.OperatingSystem -port $PORT -image $SERVERIMAGE -heapSize $HEAPSIZE -freeHeap $FREEHEAP -stackSize $STACKSIZE -arg filename:$MOSAICFILENAME -arg user:"$USERNAME" -arg home:"$XMFHOME" -arg license:license.lic -arg projects:"$XMFPROJECTS" -arg doc:"$XMFDOC" -arg version:"$VERSION" %3 %4 %5 %6 %7 %8 %9
+java $MAXJAVAHEAP -cp $LIB XOS.OperatingSystem -port $PORT -image $SERVERIMAGE -heapSize $HEAPSIZE -freeHeap $FREEHEAP -stackSize $STACKSIZE -arg filename:$MOSAICFILENAME -arg user:"$USERNAME" -arg home:"$XMFHOME" -arg license:license.lic -arg projects:"$XMFPROJECTS" -arg doc:"$XMFDOC" -arg version:"$VERSION"
 echo "Done mosaic"
