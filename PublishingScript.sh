f470a038c5528df0577a408e6c81f8ef06886c8b
phalix <sebastianbittmann@gmail.com>
2012-11-20 14:17:44 +0000

    added bitbucket as backup host

--- PublishingScript.sh
+++ PublishingScript.sh
@@ -7,6 +7,7 @@ mv $WORKSPACE/XModeler-win32.win32.x86.zip $1
 cd $1
 git add -A
 git commit -m "$(date +%s)_$RANDOM"
-echo "Pushing new files"
-git push -u origin master
+echo "Pushing new files to backup host"
+#git remote add bitbucket ssh://git@bitbucket.org/xmodeler/xmodrel.git
+git push -u bitbucket master
 cd $CURDIR
